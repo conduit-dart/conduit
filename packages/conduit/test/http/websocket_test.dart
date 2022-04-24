46c28128df31e82964838ac037459140fa9653dd
Plamen Todorov <plamen@quint8.net>
2022-04-24 12:22:22 +0300

    now it has proper cleanup

--- packages/conduit/test/http/websocket_test.dart
+++ packages/conduit/test/http/websocket_test.dart
@@ -21,46 +21,43 @@ void main() {
       return await app.stop();
     });
 
-    test("Send single message", () async {
+    test("Send single message. Await broadcast incoming stream", () async {
       final url = Uri.parse('$urlPrefix/test');
       final socket = WebSocketChannel.connect(url);
       final incoming = socket.stream.asBroadcastStream();
-      print('websocket extensions: ${socket.protocol}');
       const msg = 'this message is transfered over WebSocket connection';
       socket.sink.add(msg);
-      socket.sink.add('stop');
-      socket.sink.add('another');
+      socket.sink.add('stop'); //the server will stop the connection
 
-      var response = await incoming
-          .first; //the TestChannel should respond with hash code of the message
+      //the TestChannel should respond with hash code of the message
+      var response = await incoming.first;
       expect(response.toString(), msg.hashCode.toString());
-      //await socket.sink.close(WebSocketStatus.normalClosure, 'all is good');
-      await Future.delayed(const Duration(seconds: 1));
-      print('waiting for the socket to close');
-//      await socket.sink.close(WebSocketStatus.normalClosure, 'all is good');
       await socket.sink.done;
-      print('the socket closed with reason ${socket.closeReason}');
+      expect(socket.closeReason, 'stop acknowledged');
     });
 
     test("Send stream of messages", () async {
       final url = Uri.parse('$urlPrefix/test');
       final socket = WebSocketChannel.connect(url);
-      final messages = <String>[for (var x = 0; x < 100; ++x) 'message $x'];
-      messages.forEach(socket.sink.add);
-      socket.sink.add('stop');
+
       var i = 0;
       final stopHash = 'stop'.hashCode.toString();
-      await for (var rx in socket.stream) {
+      final messages = <String>[for (var x = 0; x < 100; ++x) 'message $x'];
+      socket.stream.listen((rx) async {
         var hash = rx.toString();
         if (hash == stopHash) {
-          break;
+          await socket.sink.done;
+        } else {
+          expect(messages[i++].hashCode.toString(),
+              rx.toString()); //check confirmation of each message
         }
+      });
 
-        expect(messages[i++].hashCode.toString(),
-            rx.toString()); //check confirmation of each message
-      }
-      await socket.sink.close(WebSocketStatus.normalClosure, 'all is good');
+      messages.forEach(socket.sink.add);
+      socket.sink.add('stop');
+
       await socket.sink.done;
+      expect(socket.closeReason, 'stop acknowledged');
     });
 
     test("chat", () async {
@@ -85,13 +82,15 @@ void main() {
       expect(data, sentMsg2);
 
       user1.sink.add('{"to" : "user2", "msg": "bye"}');
-      final farewell = await rxUser2.first;
-      expect(farewell, 'bye');
+      final lastMsg = await rxUser2.first;
+      expect(lastMsg, 'bye');
 
       await user1.sink.done;
-      print('close reason: ${user1.closeReason}');
+      expect(user1.closeReason, 'farewell user1');
+
       user2.sink.add('{"to" : "user1", "msg": "bye"}');
       await user2.sink.done;
+      expect(user2.closeReason, 'farewell user2');
     });
   });
 }
@@ -113,21 +112,23 @@ class TestChannel extends ApplicationChannel {
 }
 
 class TestController extends ResourceController {
+  final _stopwatch = Stopwatch();
   Future _processConnection(WebSocket socket) async {
     await for (var message in socket) {
-      print('inocming message $message');
+      await Future.delayed(const Duration(milliseconds: 5));
       socket.add('${message.hashCode}');
       if (message == 'stop') {
         break;
       }
     }
-    await socket.close(WebSocketStatus.normalClosure, 'request to stop');
+    await socket.close(WebSocketStatus.normalClosure, 'stop acknowledged');
     print('test controller closed the connection');
     return Future.value();
   }
 
   @Operation.get()
   Future<Response?> testMethod() {
+    _stopwatch.start();
     final httpRequest = request!.raw;
     WebSocketTransformer.upgrade(httpRequest).then(_processConnection);
     return Future.value(
@@ -146,7 +147,7 @@ class ChatController extends ResourceController {
       _socket[to]!.add(msg);
     }
     if (msg == 'bye' && _socket.containsKey(user)) {
-      _socket[user]!.close(WebSocketStatus.normalClosure, 'bye');
+      _socket[user]!.close(WebSocketStatus.normalClosure, 'farewell $user');
       _socket.remove(user);
     }
   }
