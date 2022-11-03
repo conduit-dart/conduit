963a0bc40a45d14d30462ac66424e267b1c4d928
AnhQuan Nguyen <j4qfrost@gmail.com>
2022-11-03 00:34:20 -0700

    normalize

--- packages/runtime/lib/src/analyzer.dart
+++ packages/runtime/lib/src/analyzer.dart
@@ -4,6 +4,7 @@ import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:path/path.dart';
 
 class CodeAnalyzer {
   CodeAnalyzer(this.uri) {
@@ -118,10 +119,8 @@ class CodeAnalyzer {
       }
       // ignore: empty_catches
     } catch (e) {}
-    final unit = contexts
-            .contextFor(path)
-            .currentSession
-            .getParsedUnit(fileUri.toFilePath(windows: Platform.isWindows))
+    final unit = contexts.contextFor(path).currentSession.getParsedUnit(
+            normalize(fileUri.toFilePath(windows: Platform.isWindows)))
         as ParsedUnitResult;
     return unit.unit;
   }
