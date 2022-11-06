02dc99dd8bfea563fbe42297416e6dea7f4cef02
AnhQuan Nguyen <j4qfrost@gmail.com>
2022-11-05 22:20:58 -0700

    test

--- packages/isolate_exec/lib/src/executor.dart
+++ packages/isolate_exec/lib/src/executor.dart
@@ -67,7 +67,7 @@ class IsolateExecutor<U> {
         "data:application/dart;charset=utf-8,$scriptSource",
       );
 
-      print(message['migrations']?[0].keys);
+      print(message['migrations']?[0]['uri']?.runtimeType);
 
       await Isolate.spawnUri(
         dataUri,
