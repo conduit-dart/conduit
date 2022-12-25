c467f95ee0f4db7a547cde859d1e73cbdaf3b369
AnhQuan Nguyen <j4qfrost@gmail.com>
2022-12-24 21:23:23 -0600

    test unit

--- packages/cli/test/command/create_test.dart
+++ packages/cli/test/command/create_test.dart
@@ -175,7 +175,7 @@ void main() {
         const String cmd = 'dart';
         final res = Process.runSync(
           cmd,
-          ["pub", "run", "test", "-j", "1"],
+          ["test"],
           runInShell: true,
           workingDirectory: cli.agent.workingDirectory.uri
               .resolve("test_project")
