92000a1f4157842ffa50f851259b0530b6eaceb0
AnhQuan Nguyen <j4qfrost@gmail.com>
2023-01-07 17:36:10 -0800

    add dependcy caching to build command

--- packages/cli/lib/src/cli/mixins/project.dart
+++ packages/cli/lib/src/cli/mixins/project.dart
@@ -64,6 +64,7 @@ abstract class CLIProject implements CLICommand {
       }
 
       final lockFileContents = loadYaml(lockFile.readAsStringSync()) as Map;
+      print(lockFileContents);
       final projectVersion =
           // ignore: avoid_dynamic_calls
           lockFileContents["packages"]["conduit"]["version"] as String;
