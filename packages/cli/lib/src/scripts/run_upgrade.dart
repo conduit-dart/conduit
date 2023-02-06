d737ede49eddda82baaa4364931fdf9d9ba35436
AnhQuan Nguyen <j4qfrost@gmail.com>
2023-02-06 13:46:40 -0600

    revert asMap

--- packages/cli/lib/src/scripts/run_upgrade.dart
+++ packages/cli/lib/src/scripts/run_upgrade.dart
@@ -119,7 +119,7 @@ class RunUpgradeExecutable extends Executable<Map<String, dynamic>> {
         "package:conduit_core/conduit_core.dart",
         "package:logging/logging.dart",
         "package:postgres/postgres.dart",
-        "package:conduit/src/migration_source.dart",
+        "package:conduit/conduit.dart",
         "package:conduit_runtime/runtime.dart"
       ];
 }
