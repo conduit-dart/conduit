36677b9cfc0005d12b504838f4cc1fbf0710aca6
AnhQuan Nguyen <j4qfrost@gmail.com>
2025-01-05 21:02:22 -0800

    simplify further

--- packages/cli/templates/db/lib/controller/simple_controller.dart
+++ packages/cli/templates/db/lib/controller/simple_controller.dart
@@ -1,12 +1,12 @@
 import 'package:wildfire/wildfire.dart';
 
-class SimpleController extends ResourceController {
+class SimpleController extends Controller {
   SimpleController(this.context);
 
   final ManagedContext context;
 
-  @Operation.get()
-  Future<Response> getAll() async {
+  @override
+  Future<RequestOrResponse> handle(Request request) async {
     return Response.ok({"key": "value"});
   }
 }
